da9c8ebb3e9eceee5560c519418b121f5ef96839
DiegoNovais01 <diegonovaes531@gmail.com>
2025-11-25 17:42:33 -0300

    Atividade Mobile
    So corrigi o card que tava colado no outro
    25/11/2025

--- app/Tarefa.jsx
+++ app/Tarefa.jsx
@@ -286,7 +286,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.05,
     shadowRadius: 4,
-    elevation: 2
+    elevation: 2,
+    margin: 4
   },
   todoLeft: {
     flex: 1,
